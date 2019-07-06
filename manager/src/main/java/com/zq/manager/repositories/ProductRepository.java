24308ea6bcdad6ce62a848b0ce3a37667bc9ecac
zq <409485369@qq.com>
2019-07-06 09:43:48 +0800

    zq

---
+++ manager/src/main/java/com/zq/manager/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.zq.manager.repositories;
+
+import com.zq.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProductRepository extends JpaRepository<Product,String>, JpaSpecificationExecutor<Product> {
+    
+}
